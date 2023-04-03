04cb77bbe2a813465739d80a15db57c21ec1788b
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-03 18:43:00 -0300

    :bug: [fix] front-end-container-now-starts

--- app/backend/src/database/models/teams.model.ts
+++ app/backend/src/database/models/teams.model.ts
@@ -23,15 +23,4 @@ Teams.init({
   timestamps: false,
 });
 
-/**
-    * `Workaround` para aplicar as associations em TS:
-    * Associations 1:N devem ficar em uma das instâncias de modelo
-    * */
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-
 export default Teams;
