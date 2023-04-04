f8e6fb5f095edc982b6a2bc83c992a2a38a29644
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-04 15:59:47 -0300

    :sparkles: teams-testing-findAll

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -7,6 +7,9 @@ import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
+import { Model } from 'sequelize';
+import { allTeams } from './mocks/teams.mock';
+import Teams from '../database/models/teams.model';
 
 chai.use(chaiHttp);
 
@@ -21,7 +24,15 @@ describe('1. Testa a rota /teams;', () => {
   // });
 
   describe('1.1. Sucesso na requisição.', () => {
+    it('GET /teams', async () => {
 
+      sinon.stub(Model, 'findAll').resolves(allTeams as Teams[])
+
+      const httpRes = await chai.request(app).get('/teams');
+
+      expect(httpRes.status).to.be.equal(200);
+      expect(httpRes.body).to.be.deep.equal(allTeams);
+    });
   });
 
   /**
