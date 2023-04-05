c5c2532cd6e76c3bb54504eed1b8a749e04dbc72
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-05 10:46:36 -0300

    :sparkles: [feat] teams-router-getById

--- app/backend/src/routes/teams.router.ts
+++ app/backend/src/routes/teams.router.ts
@@ -5,4 +5,5 @@ const router = Router();
 const teamsController = new TeamsController();
 
 router.get('/', teamsController.getAll);
+router.get('/:id', teamsController.getById);
 export default router;
