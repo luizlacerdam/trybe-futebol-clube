6fb489eadf1db6ab0a3ab218166e789569259b10
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-05 10:46:01 -0300

    :sparkles: [feat] teams-controller

--- app/backend/src/controllers/teams.controller.ts
+++ app/backend/src/controllers/teams.controller.ts
@@ -8,4 +8,10 @@ export default class TeamsController {
     const teamsArr = await this._teamsSevice.getAll();
     return res.status(200).json(teamsArr);
   };
+
+  public getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const team = await this._teamsSevice.getById(Number(id));
+    return res.status(200).json(team);
+  };
 }
