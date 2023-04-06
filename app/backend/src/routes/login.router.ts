066b5903c81f0be8476f830632a24acc7ad43012
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-06 17:07:00 -0300

    :sparkles: [feat] adding-middleWares-login

--- app/backend/src/routes/login.router.ts
+++ app/backend/src/routes/login.router.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
+import { loginValidation,
+  emailValidation,
+  passwordValidation } from '../middlewares/loginValidation';
 import UsersController from '../controllers/users.controller';
 
 const router = Router();
 const userController = new UsersController();
 
-router.post('/', userController.userLogin);
+router.post(
+  '/',
+  loginValidation,
+  emailValidation,
+  passwordValidation,
+  userController.userLogin,
+);
 export default router;
