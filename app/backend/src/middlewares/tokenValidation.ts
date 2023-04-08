a38e0cbcfe65ec9d7118e83b99b6033d8c928d52
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-08 12:52:13 -0300

    :sparkles: [feat] tokenValidation-middleware

---
+++ app/backend/src/middlewares/tokenValidation.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import { tokenValidation } from '../utils/tokenRelated';
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+  try {
+    const decoded = tokenValidation(token);
+    console.log(decoded);
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
