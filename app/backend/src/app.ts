d9e0e9ca72d05a56a87e5057a9319d7e80326d57
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-09 10:26:35 -0300

    :sparkles: [feat] matches-app-router

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import teamsRouter from './routes/teams.router';
 import userRouter from './routes/login.router';
+import matchesRouter from './routes/matches.router';
 
 class App {
   public app: express.Express;
@@ -14,6 +15,7 @@ class App {
     this.app.get('/', (req, res) => res.json({ ok: true }));
     this.app.use('/teams', teamsRouter);
     this.app.use('/login', userRouter);
+    this.app.use('/matches', matchesRouter);
   }
 
   private config():void {
