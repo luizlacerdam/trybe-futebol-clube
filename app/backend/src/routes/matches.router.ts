7eda3c825624eb0f10d15c10b247c383bf5804a6
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-10 09:37:44 -0300

    :sparkles: [feat] finishMatch-router

--- app/backend/src/routes/matches.router.ts
+++ app/backend/src/routes/matches.router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/matches.controller';
+import tokenValidation from '../middlewares/tokenValidation';
 
 const router = Router();
 const matchesController = new MatchesController();
 
 router.get('/', matchesController.getMatches);
+router.patch('/:id/finish', tokenValidation, matchesController.finishMatch);
 export default router;
