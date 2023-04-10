a8e57c3bd48bd63abd28ddd139482015f1db473d
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-10 18:55:23 -0300

    :test_tube: [test] team-testing-req-fail

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -15,33 +15,28 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('1. Testa a rota /teams;', () => {
-  // describe('1.1. Error na requisição;', () => {
-  //   it('GET /teams', async () => {
-  //     const httpRes = await chai.request(app).get('/teams');
-  //   });
-
-  // });
-
-  describe('1.1. Sucesso na requisição.', () => {
-    it('GET /teams', async () => {
-
+describe('Testa com a rota /teams;', () => {
+  afterEach(sinon.restore)
+  describe('1. Testa requisições na rota /team ;', () => {
+    it('1.1. Testa GET /teams', async () => {
       sinon.stub(Model, 'findAll').resolves(allTeams as Teams[])
-
       const httpRes = await chai.request(app).get('/teams');
-
       expect(httpRes.status).to.be.equal(200);
       expect(httpRes.body).to.be.deep.equal(allTeams);
     });
-    it('GET /teams:id', async () => {
-
+  });
+  describe('2. Testa requisições na rota /team:id', () => {
+    it('2.1. Testa GET /teams:id com sucess', async () => {
       sinon.stub(Model, 'findByPk').resolves(allTeams[15] as Teams)
-
       const httpRes = await chai.request(app).get('/teams/16');
-
       expect(httpRes.status).to.be.equal(200);
       expect(httpRes.body).to.be.deep.equal(saoPaulo);
     });
-  });
-
+    it('2.2. Testa GET /teams:id', async () => {
+      sinon.stub(Model, 'findByPk').resolves(null)
+      const httpRes = await chai.request(app).get('/teams/16');
+      expect(httpRes.status).to.be.equal(404);
+      expect(httpRes.body).to.be.deep.equal('Not found.');
+    });
+  })
 });
