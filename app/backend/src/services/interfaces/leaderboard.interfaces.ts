f2bc115bf93fc71741b803301844d8984a37bc95
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-10 22:53:38 -0300

    :sparkles: [feat] leadboard-interfaces

---
+++ app/backend/src/services/interfaces/leaderboard.interfaces.ts
@@ -0,0 +1,10 @@
+export default interface ILeader {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  tptalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+}
