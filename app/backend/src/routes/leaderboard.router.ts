cc8de2dc7e02851b3934245d411674c9bde371a0
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-13 16:13:40 -0300

    :sparkles: [feat] getTeamsPerformanceGeral-router

--- app/backend/src/routes/leaderboard.router.ts
+++ app/backend/src/routes/leaderboard.router.ts
@@ -4,6 +4,7 @@ import LeadboardController from '../controllers/leaderboard.controller';
 const router = Router();
 const leaderboardController = new LeadboardController();
 
+router.get('/', leaderboardController.getTeamsPerfomanceGeral);
 router.get('/home', leaderboardController.getTeamsPerfomanceHome);
 router.get('/away', leaderboardController.getTeamsPerfomanceAway);
 
