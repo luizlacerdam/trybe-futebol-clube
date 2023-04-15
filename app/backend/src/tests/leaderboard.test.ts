723879a494f5f8300615df92849a8d4ba6f1a702
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-15 15:51:45 -0300

    :test_tube: [test] leaderboard-away-geral-testing

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -9,7 +9,7 @@ import { allTeams } from './mocks/teams.mock';
 import Teams from '../database/models/teams.model';
 import { matchesMock } from './mocks/matches.mock';
 import Matches from '../database/models/matches.model';
-import { leaderboardHome } from './mocks/leaderboard.mock';
+import { leaderboardAway, leaderboardGeral, leaderboardHome } from './mocks/leaderboard.mock';
 
 chai.use(chaiHttp);
 
@@ -18,13 +18,33 @@ const { expect } = chai;
 describe('Testes em /leaderboard :', () => {
     afterEach(sinon.restore);
     describe('1. Testes em /leaderboard/home :', () => {
-        it('Testa se retornar todos os teams home com status 200 :', async () => {
+        it('1.1. Testa se retornar todos os teams home com status 200 :', async () => {
             // sinon.stub(Model, 'findAll').resolves(allTeams as Teams[]);
             // sinon.restore
             // sinon.stub(Model, 'findAll').resolves(matchesMock as Matches[]);
             const httpRes = await chai.request(app).get('/leaderboard/home');
             expect(httpRes.status).to.be.equal(200);
             expect(httpRes.body).to.be.deep.equal(leaderboardHome);
+        })
+    })
+    describe('2. Testes em /leaderboard/away :', () => {
+        it('2.1. Testa se retornar todos os teams home com status 200 :', async () => {
+            // sinon.stub(Model, 'findAll').resolves(allTeams as Teams[]);
+            // sinon.restore
+            // sinon.stub(Model, 'findAll').resolves(matchesMock as Matches[]);
+            const httpRes = await chai.request(app).get('/leaderboard/away');
+            expect(httpRes.status).to.be.equal(200);
+            expect(httpRes.body).to.be.deep.equal(leaderboardAway);
+        })
+    })
+    describe('3. Testes em /leaderboard :', () => {
+        it('3.1. Testa se retornar todos os teams home com status 200 :', async () => {
+            // sinon.stub(Model, 'findAll').resolves(allTeams as Teams[]);
+            // sinon.restore
+            // sinon.stub(Model, 'findAll').resolves(matchesMock as Matches[]);
+            const httpRes = await chai.request(app).get('/leaderboard');
+            expect(httpRes.status).to.be.equal(200);
+            expect(httpRes.body).to.be.deep.equal(leaderboardGeral);
         })
     })
 })
