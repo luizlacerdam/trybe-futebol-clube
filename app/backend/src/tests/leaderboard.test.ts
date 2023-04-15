0760169b1be9a699e5a9891164a3aeb638bdb64f
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-15 15:48:29 -0300

    :test_tube: [test] leaderboard-home-testing

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,30 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import { Model } from 'sequelize';
+import { allTeams } from './mocks/teams.mock';
+import Teams from '../database/models/teams.model';
+import { matchesMock } from './mocks/matches.mock';
+import Matches from '../database/models/matches.model';
+import { leaderboardHome } from './mocks/leaderboard.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testes em /leaderboard :', () => {
+    afterEach(sinon.restore);
+    describe('1. Testes em /leaderboard/home :', () => {
+        it('Testa se retornar todos os teams home com status 200 :', async () => {
+            // sinon.stub(Model, 'findAll').resolves(allTeams as Teams[]);
+            // sinon.restore
+            // sinon.stub(Model, 'findAll').resolves(matchesMock as Matches[]);
+            const httpRes = await chai.request(app).get('/leaderboard/home');
+            expect(httpRes.status).to.be.equal(200);
+            expect(httpRes.body).to.be.deep.equal(leaderboardHome);
+        })
+    })
+})
