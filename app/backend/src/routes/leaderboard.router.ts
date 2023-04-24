b9e79fe272ce115c049d09ef55ae08cf176125f0
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-24 16:37:55 -0300

    :recycle: [refa] leaderboard-Router

--- app/backend/src/routes/leaderboard.router.ts
+++ app/backend/src/routes/leaderboard.router.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import LeadboardService from '../services/leaderboard.service';
 import LeadboardController from '../controllers/leaderboard.controller';
+import MatchesModel from '../database/models/matches.model';
+import TeamsModel from '../database/models/teams.model';
 
 const router = Router();
-const leaderboardController = new LeadboardController();
 
-router.get('/', leaderboardController.getTeamsPerfomanceGeral);
-router.get('/home', leaderboardController.getTeamsPerfomanceHome);
-router.get('/away', leaderboardController.getTeamsPerfomanceAway);
+const leaderboardService = new LeadboardService(MatchesModel, TeamsModel);
+const leaderboardController = new LeadboardController(leaderboardService);
+
+router.get('/', leaderboardController.getTeamsPerfomanceGeral.bind(leaderboardController));
+router.get('/home', leaderboardController.getTeamsPerfomanceHome.bind(leaderboardController));
+router.get('/away', leaderboardController.getTeamsPerfomanceAway.bind(leaderboardController));
 
 export default router;
